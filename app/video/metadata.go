4c407ab8bd267e993f22ec4d7f8f9f47958255f1
johannes <>
2024-04-09 23:43:33 +0200

    added test for metadata finder

--- app/video/metadata.go
+++ app/video/metadata.go
@@ -1,13 +1,10 @@
 package video
 
 import (
-	"fmt"
-
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
-func GetMetadata(inputFilePath string) (error) {
+func GetMetadata(inputFilePath string) (string, error) {
 	probeData, _ := ffmpeg.Probe(inputFilePath)
-    fmt.Printf("%+v\n", (probeData))
-	return nil
+	return probeData, nil
 }
