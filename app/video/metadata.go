491bb6e2e11b8294ce286d065473999392946828
johannes <>
2024-04-10 19:39:34 +0200

    added func to set video thumbnail

--- app/video/metadata.go
+++ app/video/metadata.go
@@ -4,7 +4,16 @@ import (
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
-func GetMetadata(inputFilePath string) (string, error) {
-	probeData, _ := ffmpeg.Probe(inputFilePath)
-	return probeData, nil
+func SetThumbnailPicture(videoPath, imagePath, outputPath string) error {
+	// performs the command below with ffmpeg-go lib
+	// ffmpeg -i video.mp4 -i image.png -map 1 -map 0 -c copy -disposition:0 attached_pic out.mp4
+	videoInput := ffmpeg.Input(videoPath)
+	pictureInput := ffmpeg.Input(imagePath)
+
+	parameters := ffmpeg.KwArgs{
+		"c": "copy",
+		"disposition:0": "attached_pic",
+	}
+	
+	return ffmpeg.Output([]*ffmpeg.Stream{pictureInput, videoInput}, outputPath, parameters).Run()
 }
