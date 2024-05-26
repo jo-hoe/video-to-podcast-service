392e37ace775f3d73dcbe8d072172b9e9d5b2f0f
jo-hoe <71124804+jo-hoe@users.noreply.github.com>
2024-05-26 11:50:18 +0200

    added test for function "GetVideoDownloader"

--- app/download/vadownloader_test.go
+++ app/download/vadownloader_test.go
@@ -1,6 +1,9 @@
 package download
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_sanitizeFilename(t *testing.T) {
 	type args struct {
@@ -30,6 +33,47 @@ func Test_sanitizeFilename(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := sanitizeFilename(tt.args.filename); got != tt.want {
 				t.Errorf("sanitizeFilename() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetVideoDownloader(t *testing.T) {
+	type args struct {
+		url string
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantDownloader AudioDownloader
+		wantErr        bool
+	}{
+		{
+			name: "youtube",
+			args: args{
+				url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+			},
+			wantDownloader: &YoutubeAudioDownloader{},
+			wantErr:        false,
+		},
+		{
+			name: "unsupport url",
+			args: args{
+				url: "https://unsupport.com/123456789",
+			},
+			wantDownloader: nil,
+			wantErr:        true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotDownloader, err := GetVideoDownloader(tt.args.url)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetVideoDownloader() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotDownloader, tt.wantDownloader) {
+				t.Errorf("GetVideoDownloader() = %v, want %v", gotDownloader, tt.wantDownloader)
 			}
 		})
 	}
