ff06dfcedfbdf86bb126c62187c15152bc1738e6
jo-hoe <71124804+jo-hoe@users.noreply.github.com>
2025-01-17 22:21:03 +0100

    Refactor IP address retrieval in start-docker-compose.py to use a socket connection for better accuracy

--- start-docker-compose.py
+++ start-docker-compose.py
@@ -3,16 +3,37 @@ import subprocess
 import socket
 import argparse
 
+def get_local_ip():
+    # Create a socket that connects to an external address
+    # This helps us determine which network interface is used for external connections
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # We don't need to actually send data
+        # The connection attempt itself is enough to determine the local IP
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+        return local_ip
+    except Exception as e:
+        print(f"Error getting local IP: {e}")
+        return None
+    finally:
+        s.close()
+
 parser = argparse.ArgumentParser(description='starts docker compose and sets host IP as environment variable')
 parser.add_argument('--rebuild', dest='rebuild', action=argparse.BooleanOptionalAction,
-                        help='specifies if docker compose should be rebuilt')
+                    help='specifies if docker compose should be rebuilt')
 args = parser.parse_args()
 
-# This will get the IP address assigned to the default network interface
-host_ip = socket.gethostbyname(socket.gethostname())
+# Get the local network IP
+host_ip = get_local_ip()
+if host_ip:
+    print(f"Using local IP: {host_ip}")
+    os.environ['BASE_URL'] = host_ip
+else:
+    print("Failed to get local IP address")
+    exit(1)
 
-os.environ['BASE_URL'] = host_ip
 if args.rebuild:
     subprocess.run(["docker-compose", "up", "--build"])
-else: 
+else:
     subprocess.run(["docker-compose", "up"])
