15619c616f2015136ed8d0e9aa9b077b3481134a
johannes <>
2025-05-15 12:07:30 +0200

    :heavy_plus_sign: Add UUIDv4 hashing for audio file names and corresponding tests; fixed title generation

--- app/feed/feedservice.go
+++ app/feed/feedservice.go
@@ -1,6 +1,7 @@
 package feed
 
 import (
+	"crypto/md5"
 	"fmt"
 	"log"
 	"net/url"
@@ -102,6 +103,28 @@ func (fp *FeedService) getFeedWithAuthor(author string, feeds []*feeds.Feed) *fe
 	return nil
 }
 
+func hashFileNameToUUIDv4(filename string) string {
+	// take an audio file path and hash it to a UUIDv4
+	data := []byte(filename)
+	hash := md5.Sum(data)
+	hashBytes := hash[:]
+
+	uuid := make([]byte, 16)
+	copy(uuid, hashBytes)
+
+	// Set version (4) and variant bits according to RFC 4122
+	uuid[6] = (uuid[6] & 0x0f) | 0x40 // Version 4
+	uuid[8] = (uuid[8] & 0x3f) | 0x80 // Variant is 10
+
+	return fmt.Sprintf("%08x-%04x-%04x-%04x-%012x",
+		uuid[0:4],
+		uuid[4:6],
+		uuid[6:8],
+		uuid[8:10],
+		uuid[10:16],
+	)
+}
+
 func (fp *FeedService) createFeedItem(audioFilePath string) (*feeds.Item, error) {
 	audioMetadata, err := mp3joiner.GetFFmpegMetadataTag(audioFilePath)
 	if err != nil {
@@ -123,11 +146,13 @@ func (fp *FeedService) createFeedItem(audioFilePath string) (*feeds.Item, error)
 	}
 
 	return &feeds.Item{
-		Title:       common.ValueOrDefault(audioMetadata["Title"], fileNameWithoutExtension),
+		Title:       common.ValueOrDefault(audioMetadata["title"], fileNameWithoutExtension),
 		Link:        &feeds.Link{Href: fp.getFeedItemUrl(audioMetadata[mp3KeyAttribute], fileInfo.Name())},
 		Description: description,
 		Author:      &feeds.Author{Name: common.ValueOrDefault(audioMetadata[mp3KeyAttribute], "")},
 		Created:     uploadTime,
+		IsPermaLink: "false",
+		Id:          hashFileNameToUUIDv4(fileInfo.Name()),
 	}, nil
 }
 
