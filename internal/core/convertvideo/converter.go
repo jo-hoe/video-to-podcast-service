f04bec6146ffedf48d8215079f9997f3a688485c
johannes <>
2025-12-15 20:26:38 +0100

    removed unused files

--- internal/core/convertvideo/converter.go
+++
@@ -1,9 +0,0 @@
-package convertvideo
-
-import (
-	ffmpeg "github.com/u2takey/ffmpeg-go"
-)
-
-func ConvertVideoToAudio(videoFilePath string, outputFilePath string) error {
-	return ffmpeg.Input(videoFilePath).Output(outputFilePath).Run()
-}
